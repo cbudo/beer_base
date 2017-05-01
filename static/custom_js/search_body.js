06a1347a6634e9df3a32fdf606678be867334499
trottasn <trottasn@rose-hulman.edu>
2017-05-01 00:29:44 -0400

    full functionality other than linking

--- static/custom_js/search_body.js
+++ static/custom_js/search_body.js
@@ -1,17 +1,24 @@
 function beer_or_brewery(){
     entity = $('#entity-chooser').val();
-    console.log(entity);
     if(entity === 'beer') {
         $('#brewery_sel').show();
-        $('#ibu_sel').show();
-        $('#abv_sel').show();
-        $('#location_sel').hide();
+        $('#style_sel').show();
+        $('#category_sel').show();
+        $('#city_sel').hide();
+        $('#state_sel').hide();
+        $('#country_sel').hide();
+        $('#beerTable').show();
+        $('#breweryTable').hide();
     }
     else{
         $('#brewery_sel').hide();
-        $('#ibu_sel').hide();
-        $('#abv_sel').hide();
-        $('#location_sel').show();
+        $('#style_sel').hide();
+        $('#category_sel').hide();
+        $('#city_sel').show();
+        $('#state_sel').show();
+        $('#country_sel').show();
+        $('#beerTable').hide();
+        $('#breweryTable').show();
     }
 }
 
@@ -29,14 +36,21 @@ function search_by_keywords(){
         url: server_endpoint,
         success: function (data) {
             if (data.status_code === 200) {
-
-                $('#searchTable tbody').html(
-                    $.map(data.results, function (item, index) {
-                        return '<tr> <td>'+ cat_name(item.name) + '</td> <td>' + cat_name(item.category) + '</td> <td>'
-                        + cat_name(item.style) + '</td> <td>' + item.abv[0] + '</td> <td>' + item.ibu[0] + '</td> <td>'
-                        + cat_name(item.brewery) + '</td> </tr>';
-                    }).join());
-
+                if (entity === 'beer'){
+                    $('#beerTable tbody').html(
+                        $.map(data.results, function (item, index) {
+                            return '<tr> <td>'+ cat_name(item.name) + '</td> <td>' + cat_name(item.category) + '</td> <td>'
+                            + cat_name(item.style) + '</td> <td>' + item.abv[0] + '</td> <td>' + item.ibu[0] + '</td> <td>'
+                            + cat_name(item.brewery) + '</td> </tr>';
+                        }).join());
+                }
+                else {
+                    $('#breweryTable tbody').html(
+                        $.map(data.results, function (item, index) {
+                            return '<tr> <td>'+ cat_name(item.name) + '</td> <td>' + cat_name(item.city) + '</td> <td>'
+                            + cat_name(item.state) + '</td> <td>' + cat_name(item.country) + '</td> </tr>';
+                        }).join());
+                }
             }
             else {
                 alert("Unknown Error - Search Not Successful Due To Unforeseen Error.");
