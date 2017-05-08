285563e42a5a6f1a98504f684739b6437d6a0e03
Steve <stevenicholastrotta@gmail.com>
2017-05-08 12:31:33 -0400

    fixed little bugs - should be good to go! Ask if you've got q's

--- run_server.py
+++ run_server.py
@@ -5,6 +5,7 @@ import schedule
 import time
 import config
 import threading
+from brewery import Brewery
 
 
 def solr_is_up():
@@ -54,6 +55,9 @@ if __name__ == '__main__':
     updatr = Updatr(10)
     t = threading.Thread(target=updatr.scheduler)
     t.daemon = True
+    brewery_ex = Brewery(5000, 'brewery', '45236', "'Nati", 'OHIO', 'U.S. of A')
+    brewery_ex.submitBrewery2solr()
+    brewery_ex.deleteBreweryFromsolr()
     # t.start()
     app.run(host='0.0.0.0', port=5000, debug=True)  # Use this for production
     # curr_server.get_app().run()  # This is for local execution
